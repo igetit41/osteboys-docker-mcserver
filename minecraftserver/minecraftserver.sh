ae0c790d461c704d708d00cc52a64d19d1b76e4f
igetit41 <45726571+igetit41@users.noreply.github.com>
2025-01-01 14:21:28 -0800

    dunno

--- minecraftserver/minecraftserver.sh
+++ minecraftserver/minecraftserver.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#git -C /home/d3f1l3/osteboys-docker-mcserver pull origin main
+git -C /home/d3f1l3/osteboys-docker-mcserver reset --hard
 git -C /home/d3f1l3/osteboys-docker-mcserver pull origin main
 
 docker compose up -d
--- setup.sh
+++ setup.sh
@@ -22,28 +22,30 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 
 sudo service docker start
 
+sudo groupadd docker
 sudo usermod -a -G docker $USER
 newgrp docker
 
 
-sudo git config --global --add safe.directory '*'
+git config --global --add safe.directory '*'
 
-sudo git clone https://github.com/igetit41/osteboys-docker-mcserver
+git clone https://github.com/igetit41/osteboys-docker-mcserver
 
 #sudo git -C $GITPATH reset --hard
 #sudo git -C $GITPATH pull origin main
-
-sudo chmod +x $GITPATH/minecraftserver/minecraftserver.sh
-
-sudo cp $GITPATH/minecraftserver/minecraftserver.service /etc/systemd/system/minecraftserver.service
 
 # ATM9 SB
 #sudo curl -O https://mediafilez.forgecdn.net/files/5952/312/server-1.1.3.zip
 #sudo mv ./server-1.1.3.zip $GITPATH/minecraftserver/modpacks/server-1.1.3.zip
 
 # ATM10
-sudo curl -O https://mediafilez.forgecdn.net/files/6028/982/Server-Files-2.13.zip
-sudo mv ./Server-Files-2.13.zip $GITPATH/minecraftserver/modpacks/Server-Files-2.13.zip
+mkdir $GITPATH/minecraftserver/modpacks
+curl -O https://mediafilez.forgecdn.net/files/6028/982/Server-Files-2.13.zip
+mv ./Server-Files-2.13.zip $GITPATH/minecraftserver/modpacks/Server-Files-2.13.zip
+
+sudo chmod +x $GITPATH/minecraftserver/minecraftserver.sh
+
+sudo cp $GITPATH/minecraftserver/minecraftserver.service /etc/systemd/system/minecraftserver.service
 
 sudo systemctl daemon-reload
 sudo systemctl restart minecraftserver
